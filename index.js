ac486209b65df2b2503a334ddb999075af296f17
khadijaezzayany <khadijaezzayany8@gmail.com>
2020-03-13 14:20:43 +0100

    Update index.js
    enoncé 2
    1)2)

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
- function addFavoriteBook(bookName) {
-     bookName = ["ali", "adam", "tasnim"];
-     var book = bookName.includes("Great");
-     if (book == true) {
-         console.log(bookName);
-     } else {
-         bookName.push("Great");
-         console.log(bookName);
-     }
+//  var bookName = ["ali", "adam", "tasnim"];
 
-     function printFavoritBooks() {
-         for (let value of bookName) {
-             console.log(value);
-         }
-         var x = bookName.length;
-         console.log("Livres favoris :" + " " + x);
-     }
-     printFavoritBooks()
- }
- addFavoriteBook()
+class bookshelf {
+    constructor() {
+        this.addFavoriteBook();
+        this.printFavoritBooks();
+    }
+    addFavoriteBook(bookName) {
+        bookName = ["ali", "adam", "tasnim"];
+        var book = bookName.includes("Great");
+        if (book == true) {
+            console.log(bookName);
+        } else {
+            bookName.push("Great");
+            console.log(bookName);
+        }
+
+    }
+    printFavoritBooks(bookName) {
+        bookName = ["ali", "adam", "tasnim"];
+        for (let value of bookName) {
+            console.log(value);
+        }
+        var x = bookName.length;
+        console.log("Livres favoris :" + " " + x);
+    }
+}
+    window.onload = new bookshelf();
+
+    // function 
+    //      function 
+    //      printFavoritBooks()
+    //  }
+    //  addFavoriteBook()
