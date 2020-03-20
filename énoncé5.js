0b9c5197b371bae2d884adfe7d9453e12f1094e3
khadijaezzayany <khadijaezzayany8@gmail.com>
2020-03-20 19:15:13 +0100

    énonce5

--- énoncé5.js
+++ énoncé5.js
@@ -28,6 +28,20 @@ var collection = {
 // Completer la fonction suivante
 function updateRecords(id, prop, value) {
 
-
+    if (prop === "tracks") {
+        collection[id][prop] = collection[id][prop] || [];
+        collection[id][prop].push(value);
+    } else if (value === "") {
+        delete collection[id][prop];
+    } else {
+        collection[id][prop] = value;
+    }
     return collection;
 }
+updateRecords(5439, "artist", "ABBA");
+updateRecords(1245, "tracks", "Addicted to Love");
+updateRecords(1245, "album", "Riptide");
+updateRecords(2468, "tracks", "Free");
+updateRecords(2548, "artist", "")
+updateRecords(2548, "tracks", "");
+console.log(updateRecords(5439, "tracks", "Take a Chance on Me"));
